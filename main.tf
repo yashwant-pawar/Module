38a7533298864ffb81ed65e3244cb896013c442a
Yashwant <yashwant.pawar122@gmail.com>
2022-12-19 12:32:28 +0530

    New file added

---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_version = "1.3.6"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.47.0"
+    }
+    
+  }
+}
+provider "aws" {
+    profile = "default"
+    region = "us-east-1"
+}
+
+resource "aws_instance" "yes" {
+  ami = "ami-0b0dcb5067f052a63"
+  instance_type = "t2.Micro"
+  key_name = "LinuxKeyPair"
+  tags = {
+    "Name" = "Yashwant_New"
+    "Deparment" = "IT"
+    "costcenter" = "0000"
+    "count" = 2
+  }
+
+}
+
+resource "aws_ebs_volume" "example" {
+  availability_zone = "us-west-2a"
+  size              = 40
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
