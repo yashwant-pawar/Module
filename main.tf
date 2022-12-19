8296c6d3bc18ceb3a02c118f770e0ed8d8428310
Yashwant Pawar <120786834+yashwant-pawar@users.noreply.github.com>
2022-12-19 13:04:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,12 +26,4 @@ resource "aws_instance" "yes" {
 
 }
 
-resource "aws_ebs_volume" "example" {
-  availability_zone = "us-west-2a"
-  size              = 40
-
-  tags = {
-    Name = "HelloWorld"
-  }
-}
 
